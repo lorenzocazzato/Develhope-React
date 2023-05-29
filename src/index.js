748a6c43f2647f186638016f43c811626dba5c79
Lorenzo Cazzato <lorenzocazzato98@icloud.com>
2023-05-29 14:55:08 +0200

    esercizio5

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from "react";
 import Message from "./message";
 import Hello from "./hello";
 import ReactDOM from "react-dom";
+import Welcome from "./welcome";
 import Welcome2 from "./welcome2";
+
+const strongName = <strong>Lorenzo</strong>;
 
 ReactDOM.render(
   <div>
     <Hello />
     <Message />
-
-    <Welcome name="Lorenzo" />
+    <Welcome name={strongName} age={25} />
     <Welcome2 />
   </div>,
   document.getElementById("root")
