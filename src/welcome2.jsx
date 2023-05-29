d1a62dd7538a072498372c3d886eef9a662daaec
Lorenzo Cazzato <lorenzocazzato98@icloud.com>
2023-05-29 15:02:56 +0200

    esercizio5

--- src/welcome2.jsx
+++ src/welcome2.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export default class Welcome2 extends React.Component {
   render() {
-    return <p>Welcome {this.props.name}</p>;
+    return <p>Welcome {this.props.name2}</p>;
   }
 }
 
 Welcome2.defaultProps = {
-  name: "Lorenzo",
+  name2: "Lorenzo",
 };
