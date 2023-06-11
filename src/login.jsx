1b56e07b57385203560c7cd7e9ad6d21bbc7019b
Lorenzo Cazzato <lorenzocazzato98@icloud.com>
2023-06-11 17:50:11 +0200

    esercizio19

--- src/login.jsx
+++ src/login.jsx
@@ -1,68 +1,50 @@
 import React from "react";
 
 export default class Login extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: "",
-      password: "",
-    };
-  }
+  state = {
+    username: "",
+    password: "",
+  };
 
   handleInputChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
+    const { name, value } = event.target;
     this.setState({ [name]: value });
   };
 
-  handleButtonClick = (event) => {
-    event.preventDefault();
-    const button1 = document.querySelector("#buttsubmit");
-    if (this.state.username.length && this.state.password.length) {
-      button1.disabled = false;
-    } else {
-      button1.disabled = true;
-    }
-  };
-
-  handleButtonReset = (event) => {
-    this.setState = {
-      username: "",
-      password: "",
-    };
+  handleLogin = () => {
+    const { username, password } = this.state;
+    this.props.onLogin({ username, password });
   };
 
   render() {
+    const { username, password } = this.state;
+    const isDisabled = username === "" || password === "";
+
     return (
       <div>
-        <form id="form">
-          <label htmlFor="namein">Nome</label>
-
+        <form>
+          <label htmlFor="username">Username:</label>
           <input
-            id="namein"
             type="text"
+            id="username"
             name="username"
+            value={username}
             onChange={this.handleInputChange}
           />
-
-          <label htmlFor="passin">Password</label>
+          <label htmlFor="password">Password:</label>
           <input
-            id="passin"
             type="password"
+            id="password"
             name="password"
+            value={password}
             onChange={this.handleInputChange}
           />
-
           <button
-            id="buttsubmit"
-            disabled={!this.state.username || !this.state.password}
-            onClick={this.handleButtonClick}
+            type="button"
+            onClick={this.handleLogin}
+            disabled={isDisabled}
           >
             Login
-          </button>
-
-          <button id="buttreset" onClick={this.handleButtonReset}>
-            Reset
           </button>
         </form>
       </div>
