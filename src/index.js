0d9487ad4a4ec1d834e5afb5ca4d163dd0685bb4
Lorenzo Cazzato <lorenzocazzato98@icloud.com>
2023-06-16 09:52:24 +0200

    a

--- src/index.js
+++ src/index.js
@@ -1,16 +1,4 @@
 import React from "react";
-import Message from "./message";
-import Hello from "./hello";
 import ReactDOM from "react-dom";
-import Welcome from "./welcome";
-import Welcome2 from "./welcome2";
-import Counter from "./counter";
-import ClickCounter from "./clickcounter";
-import ClickTracker from "./clicktracker";
-import InteractiveWelcome from "./interactivewelcome";
-import Login from "./login";
-import UncontrolledLogin from "./uncontrolledlogin";
-import TodoList from "./todolist";
-import LanguageSelector from "./languageselector";
 
 ReactDOM.render(<div></div>, document.getElementById("root"));
