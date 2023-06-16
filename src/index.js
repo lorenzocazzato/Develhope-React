b979026689d516cdec06e92fe1be05ac350c07fb
Lorenzo Cazzato <lorenzocazzato98@icloud.com>
2023-06-16 18:11:10 +0200

    esercizio32

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { ClickCounter } from "./Functions/clickcounter";
 
-ReactDOM.render(
-  <div>
-    <ClickCounter />
-  </div>,
-  document.getElementById("root")
-);
+export function App() {
+  const handleCounterChange = (counter) => {
+    console.log("Counter is now :", counter);
+  };
+  return (
+    <>
+      <ClickCounter onCounterChange={handleCounterChange} />
+    </>
+  );
+}
+
+const root = document.querySelector("#root");
+
+ReactDOM.render(<App />, root);
