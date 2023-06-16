af7b896a14b654d8baf8473f09f969bd285d55fa
Lorenzo Cazzato <lorenzocazzato98@icloud.com>
2023-06-16 10:18:38 +0200

    a

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ClickCounter } from "./Functions/clickcounter";
 
-ReactDOM.render(<div></div>, document.getElementById("root"));
+ReactDOM.render(
+  <div>
+    <ClickCounter />
+  </div>,
+  document.getElementById("root")
+);
