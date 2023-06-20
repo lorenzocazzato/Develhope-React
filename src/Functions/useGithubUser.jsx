871a9bd4cf42f506a2abd7dc3ba232e47668e55b
Lorenzo Cazzato <lorenzocazzato98@icloud.com>
2023-06-20 12:43:23 +0200

    esercizio50

--- src/Functions/useGithubUser.jsx
+++ src/Functions/useGithubUser.jsx
@@ -1,8 +1,10 @@
 import useSWR from "swr";
+
 const fetcher = (url) => fetch(url).then((res) => res.json());
+
 export function useGitUser({ username }) {
   const { data, error, mutate } = useSWR(
-    `https://api.github.com/users/${username}`,
+    username ? `https://api.github.com/users/${username}` : null,
     fetcher
   );
 
