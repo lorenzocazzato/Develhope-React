0b06dd267f1f2c9101c136c6dc5094e37c145fc5
Lorenzo Cazzato <lorenzocazzato98@icloud.com>
2023-06-20 09:32:43 +0200

    esercizio43

--- src/Functions/hookcounter.jsx
+++ src/Functions/hookcounter.jsx
@@ -1,29 +1,19 @@
-import { useState, useCallback } from "react";
+import { useState } from "react";
 
 export function useCounter(initialValue = 0) {
   const [counter, setCounter] = useState(initialValue);
 
-  const handleIncrement = useCallback(
-    function handleIncrement() {
-      setCounter((c) => c + 1);
-    },
-    [initialValue]
-  );
+  function handleIncrement() {
+    setCounter((c) => c + 1);
+  }
 
-  const handleDecrement = useCallback(
-    function handleDecrement() {
-      setCounter((c) => c - 1);
-    },
-    [initialValue]
-  );
+  function handleDecrement() {
+    setCounter((c) => c - 1);
+  }
 
-  const handleReset = useCallback(
-    function handleReset() {
-      setCounter(initialValue);
-    },
-    [initialValue]
-  );
-
+  function handleReset() {
+    setCounter(initialValue);
+  }
   return {
     counter: counter,
     onIncrement: handleIncrement,
